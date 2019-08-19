e9bce8279bc14ae7784040f4a83e9afbc1d8dcd0
Francisco Castaneda <dragalichi@gmail.com>
2019-08-18 23:49:19 -0500

    Moving Submarine

---
+++ Assets/Scripts/Submarine/Submarine.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Submarine : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    [SerializeField] float speedOfRotation = 20f;
+    [SerializeField] float speedOfPropulsion = 20f;
+
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.right * speedOfPropulsion);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            audioSource.Pause();
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * speedOfRotation * Time.deltaTime);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * speedOfRotation * Time.deltaTime);
+        }
+    }
+}
