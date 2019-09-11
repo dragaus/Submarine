9fcab5cdc3a90dab0d6bc4c7393fd024f7677ff3
Francisco Castaneda <dragalichi@gmail.com>
2019-09-10 23:48:36 -0500

    Level improvement

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -8,12 +8,15 @@ public class GameManager : MonoBehaviour
     UIManager uiManager;
     public UserData data;
 
+    GameObject subamrine;
+
     bool submarineIsAlive;
 
     // Start is called before the first frame update
     void Awake()
     {
         SceneManager.LoadScene("GameUI", LoadSceneMode.Additive);
+        subamrine = FindObjectOfType<Submarine>().gameObject;
     }
 
     public void Initilization(UIManager manager)
@@ -31,18 +34,6 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void EndGame()
-    {
-        StartCoroutine(DeadRoutine());
-    }
-
-    public IEnumerator DeadRoutine()
-    {
-        submarineIsAlive = false;
-        data.currentGame.currentLives--;
-        yield return new WaitForSeconds(5f);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
 
     void Update()
     {
@@ -51,4 +42,51 @@ public class GameManager : MonoBehaviour
             data.currentGame.currentTotalTime += Time.deltaTime;    
         }
     }
+
+    #region Submarine Dead
+
+    //This is the public function call in the submarine to start the dead routine
+    public void EndGame()
+    {
+        StartCoroutine(DeadRoutine());
+    }
+
+    //This is the corutine call when the submarine hits something
+    public IEnumerator DeadRoutine()
+    {
+        TakeOneLife();
+        yield return new WaitUntil(() => subamrine.GetComponent<Rigidbody>().IsSleeping());
+        DestroySubamrine(out ParticleSystem explosionEffect);
+        yield return new WaitUntil(() => !explosionEffect.isPlaying);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    //We take one life of the player
+    void TakeOneLife()
+    {
+        submarineIsAlive = false;
+        data.currentGame.currentLives--;
+
+        //This is set if the player wants to accelerate the proces of the destruction
+        uiManager.SetNextButton(()=> 
+        {
+            subamrine.GetComponent<Rigidbody>().Sleep();
+        });
+    }
+
+    //We create the effect of destruction
+    void DestroySubamrine(out ParticleSystem explosionEffect)
+    {
+        //Creation of the explosion effect and set in the correct position
+        explosionEffect = Instantiate(Resources.Load<GameObject>("Effects/Explosion/Explosion")).GetComponentInChildren<ParticleSystem>();
+        explosionEffect.transform.position = subamrine.transform.position;
+
+        //Disable the subamrine to avoid the explosion effect hit the submarine
+        subamrine.gameObject.SetActive(false);
+
+        //Disable next button
+        uiManager.HideNextButton();
+    }
+
+    #endregion
 }
--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     GameManager manager;
 
-    [SerializeField]Text timeText;
+    [SerializeField] Text timeText;
     [SerializeField] Text livesAmountText;
+    [SerializeField] Button nextButton;
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +18,29 @@ public class UIManager : MonoBehaviour
         manager = FindObjectOfType<GameManager>();
         manager.Initilization(this);
         livesAmountText.text = $"X {manager.data.currentGame.currentLives}";
+        HideNextButton();
     }
 
     private void Update()
     {
         timeText.text = manager.data.currentGame.currentTotalTime.ToString("00");
     }
+
+    #region ButtonCalls
+    public void SetNextButton(UnityAction functionOfButton)
+    {
+        nextButton.onClick.AddListener(() =>
+        {
+            functionOfButton();
+            HideNextButton();
+        });
+        nextButton.gameObject.SetActive(true);
+    }
+
+    public void HideNextButton()
+    {
+        nextButton.gameObject.SetActive(false);
+    }
+
+    #endregion
 }
